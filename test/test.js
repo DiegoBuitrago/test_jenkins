4ea327aee4db83efaa6d1d31a73085a22ccb4dd0
DiegoBuitrago <alejobq@hotmail.com>
2021-05-10 15:39:13 -0500

    commit sample unit test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
-var request = require('supertest');
-var app = require('../index.js');
+// var request = require('supertest');
+// var app = require('../index.js');
 
 var supertest = require("supertest");
 var should = require("should");
 
 var server = supertest.agent("http://localhost:3000");
 
-describe('GET /', function () {
-    it('responde con hola mundo', function (done) {
-        request(app).get('/').expect("hola mundo", done)     
-    });
-});
+// describe('GET /', function () {
+//     it('responde con hola mundo', function (done) {
+//         request(app).get('/').expect("hola mundo", done)     
+//     });
+// });
 
 // describe("Responde con hola mundo mío", function () {
 //     it('responde con hola mundo Guerrero', function (hecho) {
@@ -19,20 +19,20 @@ describe('GET /', function () {
 //     });
 // });
 
-// describe("SAMPLE unit test", function () {
-//     // #1 should return home page
-//     it("should return home page", function (done) {
-//         // calling home page
-//         server
-//             .get("/")
-//             .expect("Content-type", /text/)
-//             .expect(200) // THis is HTTP response
-//             .end(function (err, res) {
-//                 console.log(res)
-//                 // HTTP status should be 200
-//                 res.status.should.equal(200);
+describe("SAMPLE unit test", function () {
+    // #1 should return home page
+    it("should return home page", function (done) {
+        // calling home page
+        server
+            .get("/")
+            .expect("Content-type", /text/)
+            .expect(200) // THis is HTTP response
+            .end(function (err, res) {
+                // console.log(res)
+                // HTTP status should be 200
+                res.status.should.equal(200);
                 
-//                 done();
-//             });
-//     });
-// });
+                done();
+            });
+    });
+});
