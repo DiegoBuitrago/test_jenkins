03e3dfef56cdea170612445279b38bfa74b2b48f
DiegoBuitrago <alejobq@hotmail.com>
2021-05-09 19:24:11 -0500

    add exit in test

--- test/test.js
+++ test/test.js
@@ -5,5 +5,6 @@ describe('GET /', function () {
     it('responde con hola mundo', function (hecho) {
         // navega a la raíz y verifica que la respuesta sea "hola mundo" 
         request(app).get('/').expect("hola mundo", hecho);
+        --exit
     });
 });
