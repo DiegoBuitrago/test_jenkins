87ad53bed5902539e40c503f8a2fa5ad58aca236
DiegoBuitrago <alejobq@hotmail.com>
2021-05-10 16:56:50 -0500

    este ultimo

--- test/test.js
+++ test/test.js
@@ -1,38 +1,14 @@
-// var request = require('supertest');
-// var app = require('../index.js');
-
 var supertest = require("supertest");
 var should = require("should");
 
 var server = supertest.agent("http://localhost:3000");
 
-// describe('GET /', function () {
-//     it('responde con hola mundo', function (done) {
-//         request(app).get('/').expect("hola mundo", done)     
-//     });
-// });
-
-// describe("Responde con hola mundo mío", function () {
-//     it('responde con hola mundo Guerrero', function (hecho) {
-//         request(app).get('/qw').expect("hola mundo Guerrero", hecho);
-//         done();
-//     });
-// });
-
-describe("SAMPLE unit test", function () {
-    // #1 should return home page
-    it("should return home page", function (done) {
-        // calling home page
+describe('Run 2', function() {
+    it('responds with json', function(done) {
         server
-            .get("/")
-            .expect("Content-type", /text/)
-            .expect(200) // THis is HTTP response
-            .end(function (err, res) {
-                // console.log(res)
-                // HTTP status should be 200
-                res.status.should.equal(200);
-                
-                done();
-            });
+        .get('/')
+        .set('Accept', 'application/text')
+        .expect('Content-Type', /text/)
+        .expect('hola mundo', done)
     });
 });
